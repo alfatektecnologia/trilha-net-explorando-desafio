e969695db849238ab12ff7385a791e26f4e54fef
Emanoel <oliveiraemanoel.br@gmail.com>
2023-12-27 22:35:45 -0300

    Added necessary code to hotel challange

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,14 +17,13 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+               throw new Exception("A quantidade de hóspedes não pode exceder a capacidade da suíte");
             }
         }
 
@@ -35,23 +34,20 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
             decimal valor = 0;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor = (Suite.ValorDiaria * DiasReservados) - (Suite.ValorDiaria * DiasReservados * 0.10M );
+            }
+            else
+            {
+                valor = Suite.ValorDiaria * DiasReservados;
             }
 
             return valor;
